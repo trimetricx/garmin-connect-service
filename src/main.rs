8c6ecc73db6a5a2ba02ddaeaa66505107b05f4d2
pc <pc@pcs-MacBook-Pro.local>
2026-02-20 22:40:07 +0100

    add utoipa

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use axum::{Router, routing::get};
+use utoipa_axum::{routes, router::OpenApiRouter};
+use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
-
-    // run it
+    let (router, api) = OpenApiRouter::new()
+    .routes(routes!(handler))
+    .split_for_parts();
+    let router = router
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
-    let _ = axum::serve(listener, app).await;
+    let _ = axum::serve(listener, router.into_make_service()).await;
 }
-
+#[utoipa::path(get, path= "/user", responses((status = OK, body = String)))]
 async fn handler() -> String {
     String::from("hello")
 }

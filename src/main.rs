70fae6a5598ba65fa0a85492bcd694a7b2b49794
pc <pc@pcs-MacBook-Pro.local>
2026-02-20 22:15:30 +0100

    axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    let _ = axum::serve(listener, app).await;
+}
+
+async fn handler() -> String {
+    String::from("hello")
 }
